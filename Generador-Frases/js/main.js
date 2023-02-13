4bd54917ea5891fee5f2fedebb273017895bf4ab
alejandroplt <alejandro.platas.02@gmail.com>
2023-02-12 23:41:05 -0600

    generador frases

--- Generador-Frases/js/main.js
+++ Generador-Frases/js/main.js
@@ -35,16 +35,3 @@ $("#buttn").on("click", function() {
         $("#sub").html(data.quoteAuthor);
     });
 });
-
-
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://frasedeldia.azurewebsites.net/api/phrase');
-xhr.onload = function() {
-    if (xhr.status === 200) {
-        console.log(xhr.responseText);
-    }
-    else {
-        console.log('Error');
-    }
-};
-xhr.send();

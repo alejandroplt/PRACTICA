5a244b3a0e32fc1c2908ef04abf288b1ae3203af
alejandroplt <alejandro.platas.02@gmail.com>
2023-02-12 23:39:27 -0600

    generador frases

---
+++ Generador-Frases/js/main.js
@@ -0,0 +1,50 @@
+function generarNuevoColor(){
+	var simbolos, color;
+	simbolos = "0123456789ABCDEF";
+	color = "#";
+
+	for(var i = 0; i < 6; i++){
+		color = color + simbolos[Math.floor(Math.random() * 10)];
+	}
+
+	document.body.style.background = color;
+    document.getElementById("sp").style.color= color;
+    document.getElementById("sub").style.color= color;
+    document.getElementById("buttn").style.backgroundColor= color;
+    document.getElementById("buttn").style.border= color;
+    document.getElementById("link").style.color= color;
+}
+$( document ).ready( function() {
+    $.ajaxSetup({
+        cache: false
+    });
+
+    $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&&format=jsonp&jsonp=?&lang=en", function(data) {
+        $("#sp").html(data.quoteText);
+        $("#sub").html(data.quoteAuthor);
+    });
+} )
+
+$("#buttn").on("click", function() {
+    $.ajaxSetup({
+        cache: false
+    });
+
+    $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&&format=jsonp&jsonp=?&lang=en", function(data) {
+        $("#sp").html(data.quoteText);
+        $("#sub").html(data.quoteAuthor);
+    });
+});
+
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://frasedeldia.azurewebsites.net/api/phrase');
+xhr.onload = function() {
+    if (xhr.status === 200) {
+        console.log(xhr.responseText);
+    }
+    else {
+        console.log('Error');
+    }
+};
+xhr.send();
